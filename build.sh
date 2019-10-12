8fa6f6bc064e9369d139ef3309c3021df1c9af33
Thomas Kruse <tk@trion.de>
2019-10-12 19:37:50 +0200

    experiment for multi arch

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# cd docker-ng-cli
+
+git checkout master
+git pull --rebase
+git checkout -b ${VERSION}
+sed -r -i "s@(.*)NG_CLI_VERSION=.*@\1NG_CLI_VERSION=${VERSION}@g" Dockerfile*
+git commit -a -m "update to ${VERSION}"
+git checkout master
+${MASTER} && git merge ${VERSION}
+git push -u origin ${VERSION}
+cd ..
+
+echo "Waiting for build docker-ng-cli"
+sleep 20m
+echo "Build should be done"
+
+cd docker-ng-cli-karma
+git checkout master
+git pull --rebase
+git checkout -b ${VERSION}
+sed -i -r "s@(.*)trion/ng-cli:.*@\1trion/ng-cli:${VERSION}@g" Dockerfile
+git commit -a -m "update to ${VERSION}"
+git checkout master
+${MASTER} && git merge ${VERSION}
+git push -u origin ${VERSION}
+cd ..
+
+echo "Waiting for build docker-ng-cli-karma"
+sleep 13m
+echo "Build should be done"
+
+cd docker-ng-cli-e2e
+git checkout master
+git pull --rebase
+git checkout -b ${VERSION}
+sed -i -r "s@(.*)trion/ng-cli-karma:.*@\1trion/ng-cli-karma:${VERSION}@g" Dockerfile
+git commit -a -m "update to ${VERSION}"
+git checkout master
+${MASTER} &&  git merge ${VERSION}
+git push -u origin ${VERSION}
+cd ..
+
+echo "Pushing latest..."
+cd docker-ng-cli
+git push --all
+cd ..
+cd docker-ng-cli-karma
+git push --all
+cd ..
+cd docker-ng-cli-e2e
+git push --all
+cd ..
